3d90687b8e9fcc9c5c0490cd6d9cdbf21759db69
iswastik3k <iswastik.3k@gmail.com>
2026-06-15 15:24:20 +0530

    test(parser): add unit test for parser correctness

---
+++ tests/parser_tests.cpp
@@ -0,0 +1,16 @@
+#include "parser/simple_feed_parser.cpp"
+#include <cassert>
+#include <iostream>
+
+int main() {
+    parser::SimpleFeedParser parser;
+    uint8_t buffer[sizeof(parser::Tick)]{};
+    parser::Tick sample{123456789ULL, 42, 100.25, 10};
+    std::memcpy(buffer, &sample, sizeof(sample));
+
+    auto ticks = parser.parse(buffer, sizeof(buffer));
+    assert(ticks.size() == 1);
+    assert(ticks[0].symbol_id == 42);
+    std::cout << "Phase 03 parser test passed." << std::endl;
+    return 0;
+}
